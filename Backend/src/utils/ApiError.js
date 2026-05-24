de79d7acd380f99c5d8d86857491ee3dc6d51e39
Cao Hoang Phuc <caophuc1612@gmail.com>
2026-05-24 22:31:54 +0700

    Utility functions - Part 1 (Error handling)

---
+++ Backend/src/utils/ApiError.js
@@ -0,0 +1,26 @@
+class ApiError extends Error {
+  constructor(statusCode, message, details = undefined) {
+    super(message);
+    this.statusCode = statusCode;
+    this.details = details;
+    this.isApiError = true;
+  }
+
+  static badRequest(msg = "Bad request", details) {
+    return new ApiError(400, msg, details);
+  }
+  static unauthorized(msg = "Unauthorized") {
+    return new ApiError(401, msg);
+  }
+  static forbidden(msg = "Forbidden") {
+    return new ApiError(403, msg);
+  }
+  static notFound(msg = "Not found") {
+    return new ApiError(404, msg);
+  }
+  static conflict(msg = "Conflict", details) {
+    return new ApiError(409, msg, details);
+  }
+}
+
+module.exports = ApiError;
